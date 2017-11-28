7d52f726aaff758dfa955c4b5207a6a8baa2cb38
Phillip Raffnsøe <phillip@praffn.dk>
2017-11-28 13:12:23 +0100

    add remaining entities as dbsets to context

--- Yaba.Entities/IYabaDBContext.cs
+++ Yaba.Entities/IYabaDBContext.cs
@@ -9,9 +9,19 @@ namespace Yaba.Entities
 {
     public interface IYabaDBContext : IDisposable
     {
+        // Budget-related entities:
         DbSet<Budget> Budgets { get; set; }
+        DbSet<Category> Categories { get; set; }
+        DbSet<Income> Incomes { get; set; } 
+        DbSet<Expense> Expenses { get; set; }
+        DbSet<Entry> Entries { get; set; }
+        DbSet<Goal> Goals { get; set; }
+        
+        // Tab-related entities
         DbSet<Tab> Tabs { get; set; }
-
+        DbSet<TabItem> TabItems { get; set; }
+        DbSet<TabCategory> TabCategories { get; set; }
+        
         Task<int> SaveChangesAsync(CancellationToken ct = default(CancellationToken));
     }
 }
