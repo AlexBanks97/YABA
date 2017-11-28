ac4f3b8f4e11eb4c8e0f5a7ba0d56cbef4a57acc
alex <alexbanks097@gmail.com>
2017-11-28 09:26:04 +0100

    Add Util test class for creating inmemory databases

---
+++ Yaba.Entities.Test/Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yaba.Entities.Test
+{
+    public class Util
+    {
+        internal static DbContextOptions<YabaDBContext> GetInMemoryDatabase()
+        {
+            return new DbContextOptionsBuilder<YabaDBContext>()
+                .UseInMemoryDatabase("in_memory_test_database")
+                .Options;
+        }
+
+        internal static IYabaDBContext GetNewContext()
+        {
+            var ctx = new YabaDBContext(GetInMemoryDatabase());
+            ctx.Database.EnsureDeleted();
+            return ctx;
+        }
+    }
+}
