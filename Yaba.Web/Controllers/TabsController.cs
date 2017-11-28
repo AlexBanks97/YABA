34092d1cb6214cba6d9eebe89b5890faf7ae274f
alex <alexbanks097@gmail.com>
2017-11-28 13:06:24 +0100

    Implemented Get() and Get(guid id)

---
+++ Yaba.Web/Controllers/TabsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Yaba.Common;
+using Yaba.Common.DTOs.TabDTOs;
+
+namespace Yaba.Web.Controllers
+{
+    public class TabsController : Controller
+    {
+        private readonly ITabRepository _repository;
+
+        public TabsController(ITabRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET api/tabs
+        [HttpGet]
+        public async Task<IEnumerable<TabDTO>> Get()
+        {
+            return await _repository.FindAllTabs();
+        }
+
+        // GET api/budgets/{guid}
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var tab = await _repository.FindTab(id);
+            if (tab == null) return NotFound(); // Returns 404
+            return Ok(tab); // Returns 200
+        }
+
+    }
+}
