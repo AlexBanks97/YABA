82858a0bee24b6ee82d22183db6c33581e2f1c64
alex <alexbanks097@gmail.com>
2017-11-29 13:38:24 +0100

    Implemented Test methods for Find(Guid id)

---
+++ Yaba.Entities.Test/EFTabItemRepositoryTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Yaba.Entities.TabEntitites;
+
+namespace Yaba.Entities.Test
+{
+    public class EFTabItemRepositoryTests
+    {
+        [Fact]
+        public async void Find_Given_Existing_Id_Returns_TabItem()
+        {
+            var context = Util.GetNewContext(nameof(Find_Given_Existing_Id_Returns_TabItem));
+
+            var tabitem = new TabItem { Amount = 42 };
+
+            context.TabItems.Add(tabitem);
+            await context.SaveChangesAsync();
+
+            using (var repo = new EFTabItemRepository(context))
+            {
+                var actual = await repo.Find(tabitem.Id);
+                Assert.Equal(42, actual.Amount);
+            }
+        }
+
+        [Fact]
+        public async void Find_Given_Nonexistent_Id_Returns_Null()
+        {
+            using (var repo = new EFTabItemRepository(Util.GetNewContext(nameof(Find_Given_Existing_Id_Returns_TabItem))))
+            {
+                var actual = await repo.Find(Guid.NewGuid());
+                Assert.Null(actual);
+            }
+        }
+    }
+}
