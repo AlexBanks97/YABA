6cea759cba3b6881cc11ca61f84485edb26cf042
Phillip Raffnsøe <phillip@praffn.dk>
2017-11-29 14:07:15 +0100

    add tests for category controller

--- Yaba.Web.Test/CategoriesControllerTests.cs
+++ Yaba.Web.Test/CategoriesControllerTests.cs
@@ -1,24 +1,146 @@
-﻿using Xunit;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using Yaba.Common;
+using Yaba.Common.DTOs.Category;
+using Yaba.Web.Controllers;
 
 namespace Yaba.Web.Test
 {
     public class CategoriesControllerTests
     {
         
-        /*[HttpGet]
-        public async Task<ICollection<CategorySimpleDto>> Get(Guid budgetId)
+        // [HttpGet]
+        // public async Task<ICollection<CategorySimpleDto>> Get(Guid budgetId)
+        [Fact]
+        public async void Get_given_budgetId_returns_collections()
+        {
+            var mock = new Mock<ICategoryRepository>();
+            var guid = Guid.NewGuid();
+            mock.Setup(r => r.FindFromBudget(guid))
+                .ReturnsAsync(new[]
+                {
+                    new CategorySimpleDto(),
+                    new CategorySimpleDto(),
+                    new CategorySimpleDto(),
+                });
 
-        [HttpGet("{categoryId}")]
-        public async Task<IActionResult> Get(Guid budgetId, Guid categoryId)
+            using (var controller = new CategoriesController(mock.Object))
+            {
+                var cats = await controller.Get(guid);
+                Assert.Equal(3, cats.Count);
+            }
+        }
+        
+        // [HttpGet("{categoryId}")]
+        // public async Task<IActionResult> Get(Guid budgetId, Guid categoryId)
+        [Fact]
+        public async void Get_given_existing_category_id_returns_Ok_with_content()
+        {
+            var mock = new Mock<ICategoryRepository>();
+            var guid = Guid.NewGuid();
+            var dto = new CategoryDetailsDto {Name = "Category"};
+            mock.Setup(r => r.Find(guid))
+                .ReturnsAsync(dto);
+            
+            using (var ctrl = new CategoriesController(mock.Object))
+            {
+                var response = await ctrl.Get(guid, guid) as OkObjectResult;
+                Assert.Equal(dto, response.Value);
+            }
+        }
 
-        [HttpPost]
-        public async Task<IActionResult> Post(Guid budgetId, [FromBody] CategoryCreateDto category)
+        [Fact]
+        public async void Get_given_nonexisting_category_id_returns_NotFound()
+        {
+            var mock = new Mock<ICategoryRepository>();
+            mock.Setup(r => r.Find(It.IsAny<Guid>()))
+                .ReturnsAsync(default(CategoryDetailsDto));
 
-        [HttpPut("{categoryId}")]
-        public async Task<IActionResult> Put(Guid budgetId, Guid categoryId, [FromBody] CategorySimpleDto category)
+            using (var ctrl = new CategoriesController(mock.Object))
+            {
+                var response = await ctrl.Get(Guid.Empty, Guid.NewGuid());
+                Assert.IsType<NotFoundResult>(response);
+            }
+        }
 
-        [HttpDelete("{categoryId}")]
-        public async Task<IActionResult> Put(Guid budgetId, Guid categoryId) */
+        // [HttpPost]
+        // public async Task<IActionResult> Post(Guid budgetId, [FromBody] CategoryCreateDto category)
+        [Fact]
+        public async void Post_given_correct_category_body_returns_CreatedAt()
+        {
+            var mock = new Mock<ICategoryRepository>();
+            mock.Setup(r => r.Create(It.IsAny<CategoryCreateDto>()))
+                .ReturnsAsync(Guid.NewGuid());
+
+            using (var ctrl = new CategoriesController(mock.Object))
+            {
+                var response = await ctrl.Post(Guid.Empty, new CategoryCreateDto {Name = ""});
+                Assert.IsType<CreatedAtActionResult>(response);
+            }   
+        }
+
+        [Fact(Skip = "Fix this!")]
+        public async void Post_given_malformed_category_body_returns_BadRequest()
+        {
+            var mock = new Mock<ICategoryRepository>();
+            using (var ctrl = new CategoriesController(mock.Object))
+            {
+                var response = await ctrl.Post(Guid.Empty, new CategoryCreateDto());
+                Assert.IsType<BadRequestResult>(response);
+            }
+        }
+
+        // [HttpPut("{categoryId}")]
+        // public async Task<IActionResult> Put(Guid budgetId, Guid categoryId, [FromBody] CategorySimpleDto category)
+        [Fact]
+        public async void Put_given_existing_cat_id_and_body_returns_NoContent()
+        {
+            var mock = new Mock<ICategoryRepository>();
+            mock.Setup(r => r.Update(It.IsAny<CategorySimpleDto>()))
+                .ReturnsAsync(true);
+
+            using (var ctrl = new CategoriesController(mock.Object))
+            {
+                var response = await ctrl.Put(Guid.Empty, Guid.NewGuid(), new CategorySimpleDto { Name = "New Name" });
+                Assert.IsType<NoContentResult>(response);
+            }
+        }
+        // WE NEED MALFORMED (BADREQUEST TEST)
+
+        // [HttpDelete("{categoryId}")]
+        // public async Task<IActionResult> Put(Guid budgetId, Guid categoryId)
+        [Fact]
+        public async void Delete_given_existing_id_returns_NoContent()
+        {
+            var mock = new Mock<ICategoryRepository>();
+            var guid = Guid.NewGuid();
+            mock.Setup(r => r.Delete(guid))
+                .ReturnsAsync(true);
+
+            using (var ctrl = new CategoriesController(mock.Object))
+            {
+                var response = await ctrl.Put(Guid.Empty, guid);
+                Assert.IsType<NoContentResult>(response);
+            }
+        }
+        
+        [Fact]
+        public async void Delete_given_nonexisting_id_returns_NotFound()
+        {
+            var mock = new Mock<ICategoryRepository>();
+            var guid = Guid.NewGuid();
+            mock.Setup(r => r.Delete(guid))
+                .ReturnsAsync(false);
+
+            using (var ctrl = new CategoriesController(mock.Object))
+            {
+                var response = await ctrl.Put(Guid.Empty, guid);
+                Assert.IsType<NotFoundResult>(response);
+            }
+        }
         
     }
 }
