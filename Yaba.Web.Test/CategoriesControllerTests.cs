20571470bc371541420bdaeed60e1237ec68ea88
Phillip Raffnsøe <phillip@praffn.dk>
2017-11-29 13:12:34 +0100

    add categories controller with empty test

---
+++ Yaba.Web.Test/CategoriesControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace Yaba.Web.Test
+{
+    public class CategoriesControllerTests
+    {
+        
+        /*[HttpGet]
+        public async Task<ICollection<CategorySimpleDto>> Get(Guid budgetId)
+
+        [HttpGet("{categoryId}")]
+        public async Task<IActionResult> Get(Guid budgetId, Guid categoryId)
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Guid budgetId, [FromBody] CategoryCreateDto category)
+
+        [HttpPut("{categoryId}")]
+        public async Task<IActionResult> Put(Guid budgetId, Guid categoryId, [FromBody] CategorySimpleDto category)
+
+        [HttpDelete("{categoryId}")]
+        public async Task<IActionResult> Put(Guid budgetId, Guid categoryId) */
+        
+    }
+}
