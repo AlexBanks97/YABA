36e430488d2ca142d474be2a230dee91acc0575f
Philip Korsholm <phko@itu.dk>
2017-11-29 13:37:59 +0100

    IBudgetEntryInterface

---
+++ Yaba.Common/IBudgetEntryRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Yaba.Common.DTOs.BudgetDTOs;
+
+namespace Yaba.Common
+{
+    public interface IBudgetEntryRepository
+    {
+        Task<ICollection<BudgetEntryDTO>> Find();
+
+        Task<ICollection<BudgetEntryDTO>> FindFromBudgetCategory(Guid BudgetCategoryId);
+
+        Task<BudgetEntryDTO> Find(Guid BudgetEntryId);
+
+        Task<Guid> CreateBudgetEntry(BudgetEntryDTO entry);
+
+        Task<bool> UpdateBudgetEntry(BudgetEntryDTO entry);
+
+        Task<bool> DeleteBudgetEntry(Guid Id);
+    }
+}
