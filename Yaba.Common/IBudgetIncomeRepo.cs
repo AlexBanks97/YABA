6d5616ca91b1b61a966389975ff4f7e27c8d394d
Mikkel <miand@itu.dk>
2017-11-29 13:54:55 +0100

    Added IBudgetIncomeRepo

---
+++ Yaba.Common/IBudgetIncomeRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Yaba.Common.DTOs.BudgetDTOs;
+
+namespace Yaba.Common
+{
+    public interface IBudgetIncomeRepo : IDisposable
+    {
+        Task<BudgetIncomeDTO> FindBudgetIncome(Guid budgetIncomeId);
+
+        Task<ICollection<BudgetIncomeDTO>> FindBudgetIncomesFromSpecificBudget(Guid BudgetId);
+
+        Task<ICollection<BudgetIncomeDTO>> FindAllBudgetIncomes();
+
+        Task<Guid> CreateBudgetIncome(BudgetIncomeDTO budgetIncome);
+
+        Task<bool> UpdateBudgetIncome(BudgetIncomeDTO budgetIncome);
+    }
+}
