9f89b2a23e57365416bce1afdd745029996895d8
alex <alexbanks097@gmail.com>
2017-11-29 13:30:40 +0100

    Add EFTabItemRepository with Dispose pattern

---
+++ Yaba.Entities/EFTabItemRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Yaba.Common;
+using Yaba.Common.DTOs.TabDTOs;
+
+namespace Yaba.Entities
+{
+    public class EFTabItemRepository : ITabItemRepository
+    {
+        private readonly IYabaDBContext _context;
+        public EFTabItemRepository(IYabaDBContext context)
+        {
+            _context = context;
+        }
+        public Task<Guid> Create(TabItemDTO tabItemDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TabItemDTO> Find(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<TabItemDTO>> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~EFTabItemRepository() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
