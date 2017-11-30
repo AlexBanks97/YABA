d51ef1f4ebc1ab2e674408106a6ecad9ed3e5794
Mikkel <miand@itu.dk>
2017-11-30 13:44:38 +0100

    changed method name in interface

--- Yaba.Common/IBudgetIncomeRepository.cs
+++ Yaba.Common/IBudgetIncomeRepository.cs
@@ -10,7 +10,7 @@ namespace Yaba.Common
     {
         Task<BudgetIncomeDTO> FindBudgetIncome(Guid budgetIncomeId);
 
-        Task<ICollection<BudgetIncomeDTO>> FindBudgetIncomesFromSpecificBudget(Guid BudgetId);
+        Task<ICollection<BudgetIncomeDTO>> FindAllBudgetIncomesFromSpecificBudget(Guid BudgetId);
 
         Task<ICollection<BudgetIncomeDTO>> FindAllBudgetIncomes();
 
