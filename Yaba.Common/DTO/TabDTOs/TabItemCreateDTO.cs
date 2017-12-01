b499bc4fdc770f2bbced0a41739f9952f4619db2
alex <alexbanks097@gmail.com>
2017-12-01 14:11:00 +0100

    Add TabItemCreateDTO and updated ITabItemRep accordingly

---
+++ Yaba.Common/DTO/TabDTOs/TabItemCreateDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yaba.Common.DTO.TabDTOs
+{
+    public class TabItemCreateDTO
+    {
+		public decimal Amount { get; set; }
+	}
+}
