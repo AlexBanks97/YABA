8ee5a19ee5617af78a296cdada1776ce4fc565a7
Mikkel <miand@itu.dk>
2017-12-01 16:12:53 +0100

    Implemented Delete method

--- Yaba.Web/Controllers/IncomeController.cs
+++ Yaba.Web/Controllers/IncomeController.cs
@@ -64,8 +64,14 @@ namespace Yaba.Web.Controllers
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(Guid incomeId)
         {
+			var deleted = await _repository.DeleteBudgetIncome(incomeId);
+			if(!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+			return NoContent();
         }
     }
 }
