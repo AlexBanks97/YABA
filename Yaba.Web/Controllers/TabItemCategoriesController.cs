09ef24e5a86c87a432f78ce700b1931e14fead58
alex <alexbanks097@gmail.com>
2017-12-02 14:49:20 +0100

    Add Controller class and Test class for Controller

---
+++ Yaba.Web/Controllers/TabItemCategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Yaba.Common.Tab;
+
+namespace Yaba.Web.Controllers
+{
+    public class TabItemCategoriesController : Controller
+    {
+		private readonly ITabItemCategoryRepository _repository;
+
+		public TabItemCategoriesController(ITabItemCategoryRepository repository)
+		{
+			_repository = repository;
+		}
+
+
+
+	}
+}
