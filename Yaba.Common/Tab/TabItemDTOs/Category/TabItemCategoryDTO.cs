e98123dd59c61f10b0e8391d4f2fa599d09026c5
alex <alexbanks097@gmail.com>
2017-12-02 13:13:47 +0100

    Let TabItemCategoryDTO have an Icollection of tab items

--- Yaba.Common/Tab/TabItemDTOs/Category/TabItemCategoryDTO.cs
+++ Yaba.Common/Tab/TabItemDTOs/Category/TabItemCategoryDTO.cs
@@ -6,7 +6,6 @@ namespace Yaba.Common.DTO.TabDTOs
 	public class TabItemCategoryDTO
 	{
 		public Guid Id { get; set; }
-		public ICollection<TabItemDTO> TabItems { get; set; }
 		public string Name { get; set; }
 	}
 }
