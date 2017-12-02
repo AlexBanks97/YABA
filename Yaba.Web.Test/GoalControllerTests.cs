e0d95e44d3c2b2ab4f52b8d45639f92aace4fa94
Philip <phko@itu.dk>
2017-12-02 14:56:16 +0100

    Added controller and test class

---
+++ Yaba.Web.Test/GoalControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Yaba.Web.Test
+{
+    public class GoalControllerTests
+    {
+		[Fact]
+		public async void Get_with_no_parameters_returns_All_Goals()
+		{
+
+		}
+	}
+}
---
+++ Yaba.Web/Controllers/GoalController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Yaba.Common.Budget;
+
+namespace Yaba.Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Goal")]
+    public class GoalController : Controller
+    {
+		IGoalRepository repository;
+
+		public GoalController(IGoalRepository repository)
+		{
+			this.repository = repository;
+		}
+
+
+		// GET: api/Goal
+		[HttpGet]
+        public Task<IActionResult> Get()
+        {
+			throw new NotImplementedException();
+        }
+
+        // GET: api/Goal/5
+        [HttpGet("{id}")]
+        public Task<IActionResult> Get(int id)
+        {
+			throw new NotImplementedException();
+		}
+        
+        // POST: api/Goal
+        [HttpPost]
+        public Task<IActionResult> Post([FromBody]string value)
+        {
+			throw new NotImplementedException();
+		}
+        
+        // PUT: api/Goal/5
+        [HttpPut("{id}")]
+        public Task<IActionResult> Put(int id, [FromBody]string value)
+        {
+			throw new NotImplementedException();
+		}
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public Task<IActionResult> Delete(int id)
+        {
+			throw new NotImplementedException();
+		}
+    }
+}
