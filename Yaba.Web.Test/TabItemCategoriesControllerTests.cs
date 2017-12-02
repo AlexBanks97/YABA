e21e5e592fccec5c6ea32fc30d488e31aed912ea
alex <alexbanks097@gmail.com>
2017-12-02 15:03:56 +0100

    Add tests for Get(Guid id)

--- Yaba.Web.Test/TabItemCategoriesControllerTests.cs
+++ Yaba.Web.Test/TabItemCategoriesControllerTests.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using Yaba.Common.DTO.TabDTOs;
+using Yaba.Common.Tab;
+using Yaba.Web.Controllers;
 
 namespace Yaba.Web.Test
 {
-    class TabItemCategoriesControllerTests
+    public class TabItemCategoriesControllerTests
     {
-    }
+		[Fact]
+		public async void Get_Given_Existing_Id_Returns_Ok()
+		{
+			var mock = new Mock<ITabItemCategoryRepository>();
+			var guid = Guid.NewGuid();
+			mock.Setup(c => c.Find(guid))
+				.ReturnsAsync(new TabItemCategoryDTO());
+
+			using (var ctrl = new TabItemCategoriesController(mock.Object))
+			{
+				var result = ctrl.Get(guid);
+				Assert.IsType<OkResult>(result);
+			}
+		}
+
+		[Fact]
+		public async void Get_Given_Non_Existing_Id_Returns_NotFound()
+		{
+			var mock = new Mock<ITabItemCategoryRepository>();
+			using (var ctrl = new TabItemCategoriesController(mock.Object))
+			{
+				var result = ctrl.Get(Guid.NewGuid());
+				Assert.IsType<NotFoundResult>(result);
+			}
+		}
+	}
 }
