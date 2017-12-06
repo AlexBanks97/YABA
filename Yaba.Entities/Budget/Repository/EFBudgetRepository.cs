9e5313aeda58fe6772181a575d2c7003cc737fb4
Christoffer Nissen <christoffer.nissen@gmail.com>
2017-12-06 19:58:36 +0100

    Changed implementation of get(it) in EFBudgetRepository

--- Yaba.Entities/Budget/Repository/EFBudgetRepository.cs
+++ Yaba.Entities/Budget/Repository/EFBudgetRepository.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Yaba.Common.Budget;
 using Yaba.Common.Budget.DTO;
+using Yaba.Common.Budget.DTO.Goal;
 using Yaba.Common.Budget.DTO.Category;
 using Yaba.Common.Budget.DTO.Recurring;
 
@@ -21,19 +22,34 @@ namespace Yaba.Entities.Budget.Repository
 
 		public async Task<BudgetDetailsDto> Find(Guid id)
 		{
-			var budget = _context.Budgets
-				.Include(b => b.Categories)
+            var budget = _context.Budgets
+                .Include(b => b.Categories).ThenInclude(c => c.GoalEntity)
 				.Include(b => b.Recurrings)
 				.FirstOrDefault(b => b.Id == id);
 			if (budget == null) return null;
+
+
+            //var categories = _context.BudgetCategories.Where(c => c.)
+
+
 			return new BudgetDetailsDto
 			{
 				Id = budget.Id,
-				Name = budget.Name,
+				Name = budget.Name, 
 
 				Categories = budget.Categories
-					.Select(c => new CategorySimpleDto { Id = c.Id, Name = c.Name} )
-					.ToList(),
+					.Select(c => new CategoryGoalDTO
+                    { 
+                    Id = c.Id, 
+                    Name = c.Name,
+
+                    Goal = c.GoalEntity == null ? null : new GoalSimpleDto{
+                        Id = c.GoalEntity.Id,
+                        Amount = c.GoalEntity.Amount,
+                        Recurrence = c.GoalEntity.Recurrence
+                    } 
+                                 
+                }).ToList(),
 
 				Recurrings = budget.Recurrings
 					.Select(i => new RecurringSimpleDto { Id = i.Id, Name = i.Name })
