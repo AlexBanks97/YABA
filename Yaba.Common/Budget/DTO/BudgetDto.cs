743001c3ac0b2b6b654102862bfd1f9da0f1fcf7
Philip <phko@itu.dk>
2017-12-06 15:59:49 +0100

    Added Data Annotations To budget DTOs

--- Yaba.Common/Budget/DTO/BudgetDto.cs
+++ Yaba.Common/Budget/DTO/BudgetDto.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using Yaba.Common.Budget.DTO.Category;
 using Yaba.Common.Budget.DTO.Income;
+
 
 namespace Yaba.Common.Budget.DTO
 {
 	public class BudgetDto
 	{
+		[Required]
 		public Guid Id { get; set; }
+
+		[Required]
+		[StringLength(50)]
 		public string Name { get; set; }
 		public ICollection<CategoryDto> Categories { get; set; }
 		public ICollection<IncomeSimpleDto> Incomes { get; set; }
