bcc56a54427aab9586a1b8c4862375d22993edf8
Philip <phko@itu.dk>
2017-12-06 16:16:08 +0100

    Budget Category Data annotations

--- Yaba.Common/Budget/DTO/Category/CategoryCreateDto.cs
+++ Yaba.Common/Budget/DTO/Category/CategoryCreateDto.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Yaba.Common.Budget.DTO.Category
 {
 	public class CategoryCreateDto
 	{
+		[Required]
+		[StringLength(50)]
 		public string Name { get; set; }
+
+		[Required]
 		public Guid BudgetId { get; set; }
 	}
 }
