08813ef1e424f82ca2f435ff9ffbbc63a4849d44
Phillip Raffnsøe <phillip@praffn.dk>
2017-12-07 14:23:37 +0100

    add remove tab item category migrations

---
+++ Yaba.Entities/Migrations/20171207132237_RemoveTabItemCategory.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Yaba.Entities.Migrations
+{
+    public partial class RemoveTabItemCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TabItems_TabItemCategories_CategoryEntityId",
+                table: "TabItems");
+
+            migrationBuilder.DropTable(
+                name: "TabItemCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TabItems_CategoryEntityId",
+                table: "TabItems");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryEntityId",
+                table: "TabItems");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CategoryEntityId",
+                table: "TabItems",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "TabItemCategories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TabItemCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TabItems_CategoryEntityId",
+                table: "TabItems",
+                column: "CategoryEntityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TabItems_TabItemCategories_CategoryEntityId",
+                table: "TabItems",
+                column: "CategoryEntityId",
+                principalTable: "TabItemCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
