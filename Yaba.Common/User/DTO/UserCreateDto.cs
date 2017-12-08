c06bfe9da8c8471bf09d55e708cb9daa0c1090e8
Mikkel <miand@itu.dk>
2017-12-08 14:26:54 +0100

    made UserCreateDto

---
+++ Yaba.Common/User/DTO/UserCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yaba.Common.User.DTO
+{
+    class UserCreateDto
+    {
+		public string name { get; set; }
+
+		public ICollection<UserDetailsDto> friends { get; set; }
+	}
+}
---
+++ Yaba.Common/User/DTO/UserSimpleDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yaba.Common.User.DTO
+{
+    class UserDetailsDto
+    {
+		public Guid Id { get; set; }
+
+		public int MyProperty { get; set; }
+	}
+}
