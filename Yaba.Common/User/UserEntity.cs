30d790d9db5da5e22d5f2e8ba7120f623439916c
Mikkel <miand@itu.dk>
2017-12-08 14:31:06 +0100

    added UserEntity

---
+++ Yaba.Common/User/UserEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yaba.Common.User
+{
+    class UserEntity
+    {
+		public Guid Id { get; set; }
+
+		public string name { get; set; }
+
+		public ICollection<UserEntity> friends { get; set; }
+	}
+}
