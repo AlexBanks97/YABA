71f64e59a0e701618b701618ab2b1a1882c20efa
Mikkel <miand@itu.dk>
2017-12-08 15:00:59 +0100

    much in doubt about which dto to use

--- Yaba.Common/User/IUserRepository.cs
+++ Yaba.Common/User/IUserRepository.cs
@@ -8,11 +8,11 @@ namespace Yaba.Common
 {
     public interface IUserRepository
     {
-		Task<UserSimpleDto> AddFriend(Guid myId, Guid otherId);
+		Task<UserDetailsDto> AddFriend(Guid myId, Guid otherId);
 
 		Task<UserCreateDto> CreateUser(UserCreateDto user);
 
-		Task<UserSimpleDto> FindUser(Guid userId);
+		Task<UserDetailsDto> FindUser(Guid userId);
 
 		Task<UserDetailsDto> FindAll();
 
