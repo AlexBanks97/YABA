989eb6111e177df554d38e37b1f590f9a85b90bb
Mikkel <miand@itu.dk>
2017-12-08 14:57:47 +0100

    added UserSimpleDto

---
+++ Yaba.Common/User/DTO/UserSimpleDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yaba.Common.User.DTO
+{
+    public class UserSimpleDto
+    {
+		public Guid Id { get; set; }
+
+		public string Name { get; set; }
+	}
+}
