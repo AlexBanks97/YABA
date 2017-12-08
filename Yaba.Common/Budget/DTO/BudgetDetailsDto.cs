6f3cb38414c6ff9d0be87f1510ae2ce5ad1ca7ff
Christoffer Nissen <christoffer.nissen@gmail.com>
2017-12-08 14:01:32 +0100

    Removed two fields from BudgetDetailsDto

--- Yaba.Common/Budget/DTO/BudgetDetailsDto.cs
+++ Yaba.Common/Budget/DTO/BudgetDetailsDto.cs
@@ -11,9 +11,7 @@ namespace Yaba.Common.Budget.DTO
 		public string Name { get; set; }
 
 		public ICollection<CategoryGoalDTO> Categories { get; set; }
-		public Decimal CategoriesTotal { get; set; }
 		public ICollection<RecurringSimpleDto> Recurrings { get; set; }
-		public Decimal RecurringsTotal { get; set; }
 		// public ICollection<BudgetExpense> Expenses { get; set; }
 	}
 }
