793794bc02e043a8e5ac4e144c58da39c1c76d86
alex <alexbanks097@gmail.com>
2017-12-09 12:10:40 +0100

    Add [ValidGuid] to TabItemCreateDTO's TabId

--- Yaba.Common/Tab/DTO/Item/TabItemCreateDTO.cs
+++ Yaba.Common/Tab/DTO/Item/TabItemCreateDTO.cs
@@ -6,6 +6,7 @@ namespace Yaba.Common.Tab.DTO.Item
     public class TabItemCreateDTO
     {
 		[Required]
+		[ValidGuid]
 		public Guid TabId { get; set; }
 		public string Description { get; set; }
 		[Required]
