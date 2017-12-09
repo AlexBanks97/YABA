db5fa96c11d8145237c90e288c5f0042b4499966
alex <alexbanks097@gmail.com>
2017-12-09 12:10:01 +0100

    Add ValidGuidAttribute class that allows [ValidGuid] data annotation

---
+++ Yaba.Common/ValidGuidAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Yaba.Common
+{
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+	public class ValidGuidAttribute : ValidationAttribute
+    {
+		public override bool IsValid(Object value)
+		{
+			bool result = true;
+
+			if ((Guid)value == Guid.Empty)
+				result = false;
+
+			return result;
+		}
+	}
+}
