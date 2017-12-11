c92387ba7a0323c769ae66537fbf356351d05861
alex <alexbanks097@gmail.com>
2017-12-11 16:44:13 +0100

    Initial GUI

--- Yaba.UWPApp/Views/HomePage.xaml.cs
+++ Yaba.UWPApp/Views/HomePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Yaba.UWPApp.Views;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +27,23 @@ namespace Yaba.UWPApp
 		public HomePage()
 		{
 			this.InitializeComponent();
+		}
+
+		private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+		{
+			var selected = args.SelectedItem as NavigationViewItem;
+			if (selected == null) return;
+
+			if (selected.Tag.ToString() == "budgets")
+			{
+				ContentFrame.Navigate(typeof(BudgetsOverview));
+			}
+			else
+			{
+				ContentFrame.Navigate(typeof(TabsOverview));
+			}
+
+
 		}
 	}
 }
