447adc7ba1b5419be6ef06e496f5231f2c47365c
Chris <christoffer.nissen@gmail.com>
2017-12-12 20:14:53 +0100

    Initial implementation with test credentials

--- Yaba.UWPApp/Views/MainPage.xaml.cs
+++ Yaba.UWPApp/Views/MainPage.xaml.cs
@@ -13,6 +13,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Auth0.OidcClient;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,10 +28,60 @@ namespace Yaba.UWPApp
         public MainPage()
         {
             this.InitializeComponent();
-        }
+			
+		}
 
-		private void Button_Click(object sender, RoutedEventArgs e)
+		public async Task<int> LoginResult()
 		{
+			/* Change once access has been granted
+		praffn.eu.auth0.com
+		mb46CJma8ApOBjibmEdJzmcEQaJn976G
+			 */
+
+			var client = new Auth0Client(new Auth0ClientOptions
+			{
+				Domain = "cntest.eu.auth0.com",
+				ClientId = "kpJCzk6eiBai0XVYQ904cgIHGG907PfD"
+			});
+
+			var loginResult = await client.LoginAsync();
+
+			if (loginResult.IsError)
+			{
+				Debug.WriteLine($"An error occurred during login: {loginResult.Error}");
+			}
+
+			if (!loginResult.IsError)
+			{
+				Debug.WriteLine($"id_token: {loginResult.IdentityToken}");
+				Debug.WriteLine($"access_token: {loginResult.AccessToken}");
+			}
+
+			// User info
+			if (!loginResult.IsError)
+			{
+				Debug.WriteLine($"name: {loginResult.User.FindFirst(c => c.Type == "name")?.Value}");
+				Debug.WriteLine($"email: {loginResult.User.FindFirst(c => c.Type == "email")?.Value}");
+			}
+
+			// Claims
+			if (!loginResult.IsError)
+			{
+				foreach (var claim in loginResult.User.Claims)
+				{
+					Debug.WriteLine($"{claim.Type} = {claim.Value}");
+				}
+			}
+
+			return 0;
+		}
+
+		
+		private async void Button_Click(object sender, RoutedEventArgs e)
+		{
+			
+			var x = await LoginResult();
+
 			this.Frame.Navigate(typeof(NavigationPage));
 		}
 	}
