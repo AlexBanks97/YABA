1949fe26a9e0fe2f223a8e073ea3394f4dbefa3c
Chris <christoffer.nissen@gmail.com>
2017-12-12 21:52:59 +0100

    Added UserGreeting upon succesfull login

--- Yaba.UWPApp/Views/MainPage.xaml.cs
+++ Yaba.UWPApp/Views/MainPage.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using Auth0.OidcClient;
 using System.Threading.Tasks;
 using System.Text;
@@ -21,18 +10,26 @@ using System.Text;
 
 namespace Yaba.UWPApp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
     {
+		String name;
         public MainPage()
         {
             this.InitializeComponent();
 			
 		}
 
-		public async Task<int> LoginResult()
+		private async void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var code = await Authorize();
+
+			if (code == 0) this.Frame.Navigate(typeof(NavigationPage), name);
+		}
+
+		public async Task<int> Authorize()
 		{
 			/* Change once access has been granted
 		praffn.eu.auth0.com
@@ -59,27 +56,24 @@ namespace Yaba.UWPApp
 				sb.AppendLine($"ID Token: {loginResult.IdentityToken}");
 				sb.AppendLine($"Access Token: {loginResult.AccessToken}");
 				sb.AppendLine($"Refresh Token: {loginResult.RefreshToken}");
-
 				sb.AppendLine();
-
 				sb.AppendLine("-- Claims --");
 				foreach (var claim in loginResult.User.Claims)
 				{
 					sb.AppendLine($"{claim.Type} = {claim.Value}");
+
+					if(claim.Type.ToString() == "name") name = claim.Value;
 				}
+
+				
 			}
+
+
 			Debug.WriteLine($"{sb}");
-
 			return i;
-		}
 
+		}
+	
 		
-		private async void Button_Click(object sender, RoutedEventArgs e)
-		{
-			
-			var x = await LoginResult();
-
-			if(x == 0) this.Frame.Navigate(typeof(NavigationPage));
-		}
 	}
 }
