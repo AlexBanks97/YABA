0859d586719bfdb7f3985e1a50b7c9545adf31a5
Philip <phko@itu.dk>
2017-12-14 16:07:57 +0100

    Added dto for stripe payments

---
+++ Yaba.Common/Payment/StripePaymentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yaba.Common.Payment
+{
+    public class StripePayment
+    {
+	    public decimal Amount { get; set; }
+	    public string Description { get; set; }
+	    public string Token { get; set; }
+    }
+}
