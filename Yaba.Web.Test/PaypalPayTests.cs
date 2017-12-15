00cd47bb96dfdfed266f9da3fd4b1b8ec690952a
Chris <christoffer.nissen@gmail.com>
2017-12-15 23:59:03 +0100

    Initial Payout implementation done

--- Yaba.Web.Test/PaypalPayTests.cs
+++ Yaba.Web.Test/PaypalPayTests.cs
@@ -28,6 +28,31 @@ namespace Yaba.Web.Test
 
             //Assert
             Assert.True(result);
+
+
         }
-    }
+
+		[Fact]
+		public void payout_Test_returns_true()
+		{
+
+			var payment = new PaymentDto
+			{
+				Amount = "100.00",
+				Description = "Test Transaction",
+				PaymentProvider = "PayPal"
+
+			};
+
+			var target = new PaypalPay();
+
+			//Act
+			var result = target.PayOut(payment);
+
+			//Assert
+			Assert.True(result);
+
+
+		}
+	}
 }
--- Yaba.Web/Payments/PaypalPay.cs
+++ Yaba.Web/Payments/PaypalPay.cs
@@ -59,5 +59,47 @@ namespace Yaba.Web.Payments
             return true;
 
         }
+
+		public bool PayOut(PaymentDto dto)
+		{
+			var config = new Dictionary<String, String>();
+			config.Add("clientId", "AVW3VSprOUqMbB3FKDrMFH2e504IO6h3Qss9LmGjq0kcfkj6glmqqD7jMCbxIIFeqGrDcy7B2dt9_u_N");
+			config.Add("clientSecret", "ECXUaE-0M5RCk3ut-enH-SFZrHMi70R8YEUgLJFS4nnd0A973fE8YPo9cuHx1jyINStSDl6P6gkjIJlI");
+			config.Add("mode", "sandbox"); // Pls dont remove
+
+			var accessToken = new OAuthTokenCredential(config).GetAccessToken();
+			var apiContext = new APIContext(accessToken);
+
+			var payout = new Payout
+			{
+				sender_batch_header = new PayoutSenderBatchHeader
+				{
+					sender_batch_id = "batch_" + System.Guid.NewGuid().ToString().Substring(0, 8),
+					email_subject = "You have a payment",
+					recipient_type = PayoutRecipientType.EMAIL
+				},
+				items = new List<PayoutItem>
+				{
+					new PayoutItem
+					{
+						recipient_type = PayoutRecipientType.EMAIL,
+						amount = new Currency
+						{
+							value = "0.00", // Set to 0 because senders balance is zero
+							currency = "DKK"
+						},
+						receiver = "christoffer.nissen-buyer@me.com",
+						note = "Thank you.",
+						sender_item_id = "item_1"
+					}
+				}
+
+			};
+
+
+			var createdPayout = payout.Create(apiContext,false);
+
+			return true;
+		}
     }
 }
