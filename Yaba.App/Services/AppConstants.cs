41c593e27a3216051aa79a9a3ec37dc8abb97e1f
= <phillip@praffn.dk>
2017-12-19 11:04:22 +0100

    make stuff work

--- Yaba.App/Services/AppConstants.cs
+++ Yaba.App/Services/AppConstants.cs
@@ -4,7 +4,7 @@ namespace Yaba.App.Services
 {
 	public class AppConstants
 	{
-		public Uri BaseApiAddress { get; } = new Uri("http://localhost:50150/api/");
+		public Uri BaseApiAddress { get; } = new Uri("https://yaba.azurewebsites.net/api/");
 		public string Auth0Domain { get; } = "praffn.eu.auth0.com";
 		public string Auth0ClientId { get; } = "DRDKy6mF0gWtMFkExlIt0HY2DvqkxPgO";
 	}
